59fdd70bea3f0398e8a4d21bb60b8d9152b3c3c1
abdo-355 <abdoluxor366@gmail.com>
2023-04-08 02:12:04 +0200

    write a form and an api endpoint to write feedbacks

--- api_app/pages/api/feedback.ts
+++ api_app/pages/api/feedback.ts
@@ -1,6 +1,38 @@
 import { NextApiHandler } from "next";
+import fs from "fs";
+import path from "path";
+
+interface IFeedback {
+  id: string;
+  email: string;
+  feedback: string;
+}
 
 const handler: NextApiHandler = (req, res) => {
+  if (req.method === "POST") {
+    const email: string = req.body.email;
+    const feedback: string = req.body.feedback;
+
+    const newFeedback: IFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback,
+    };
+
+    // TODO: store feedback in the database or a file
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+
+    const data = fs.readFileSync(filePath);
+
+    const feedbacks: IFeedback[] = JSON.parse(data as unknown as string);
+
+    feedbacks.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(feedbacks));
+
+    return res.status(201).json({ message: "Feedback saved successfully" });
+  }
+
   res.json({ message: "hi there!" });
 };
 
--- api_app/pages/index.tsx
+++ api_app/pages/index.tsx
@@ -1,9 +1,41 @@
+import { useRef, FormEventHandler } from "react";
+
 const HomePage = () => {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const feedbackRef = useRef<HTMLTextAreaElement>(null)
+
+  const submitHandler: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailRef.current?.value
+    const enteredFeedback = feedbackRef.current?.value
+
+    fetch("/api/feedback", {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: enteredEmail, feedback: enteredFeedback })
+    }).then(async response => {
+      console.log(await response.json());
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   return (
-    <h1>
-      The Home Page
-    </h1>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input ref={emailRef} type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea ref={feedbackRef} id="feedback" rows={5} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
   )
 }
 
-export default HomePage
+export default HomePage;
