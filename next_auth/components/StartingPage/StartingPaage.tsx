264c6c2ec3a374a6f251ab951a7fd91854531f8b
abdo-355 <abdoluxor366@gmail.com>
2023-04-17 01:45:11 +0200

    next auth initial commit

---
+++ next_auth/components/StartingPage/StartingPaage.tsx
@@ -0,0 +1,13 @@
+import styles from './StartingPage.module.css';
+
+const StartingPage = () => {
+    // Show Link to Login page if NOT auth
+
+    return (
+        <section className={styles.starting}>
+            <h1>Welcome on Board!</h1>
+        </section>
+    );
+}
+
+export default StartingPage;
---
+++ next_auth/pages/index.tsx
@@ -0,0 +1,7 @@
+import StartingPage from "@/components/StartingPage/StartingPaage";
+
+const HomePage = () => {
+  return <StartingPage />;
+}
+
+export default HomePage;
